3b09c4ae242cef18e6e9fc7879c6122d82333a9d
Ameya Dongare <Ameya.D@orioninc.com>
2021-05-07 18:28:17 +0530

    deleting prev

--- react-app/src/products/ProductList.js
+++ react-app/src/products/ProductList.js
@@ -1,17 +1,7 @@
-import React, {useState} from 'react';
+import React from 'react';
 import { withRouter } from 'react-router';
 
 import { CardContent } from '../components';
-
-// const [title, setTitle] = useState('Data');
-
-// const mydata = async (e) => {
-  
-//   //alert('whaaat')
-//   let res = await fetch('https://jsonplaceholder.typicode.com/todos/1');
-//   let data = await res.json();
-//   setTitle(data.title);
-// }
 
 function ProductList({products}) {
   return (
@@ -24,8 +14,7 @@ function ProductList({products}) {
               <CardContent
                 name={product.name}
                 description={product.description}
-              />
-              {/* <button type='button' onClick={mydata}>{title}</button> */}
+              />            
             </div>
           </li>
         ))}
